912f1d9c74aaa35a4ea15e254ae62ba33a8263ad
Steven Vos <steven.vos@infocaster.net>
2023-08-13 18:00:40 +0200

    STVN > Header animation + add VueUse

--- storyblok/Header.vue
+++ storyblok/Header.vue
@@ -1,16 +1,60 @@
 <script setup>
+    import { useMouseInElement } from "@vueuse/core";
+
     const props = defineProps({ blok: Object });
 
+    const headerCard = ref(null);
+
     const articleContent = computed(() => renderRichText(props.blok.Intro));
+
+    const cardTransform = computed(() => {
+        const MAX_ROTATION = 6;
+
+        const xRotation = (
+            MAX_ROTATION / 2 -
+            (elementY.value / elementHeight.value) * MAX_ROTATION
+        ).toFixed(2);
+        const yRotation = (
+            (elementX.value / elementHeight.value) * MAX_ROTATION -
+            MAX_ROTATION / 2
+        ).toFixed(2);
+
+        return isOutside.value
+            ? ""
+            : `perspective(${elementWidth.value}px) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+    });
+
+    const { elementX, elementY, isOutside, elementHeight, elementWidth } =
+        useMouseInElement(headerCard);
 </script>
 
 <template>
     <div
         v-editable="blok"
-        class="py-32 text-6xl text-[#50b0ae] font-bold text-center"
+        ref="headerCard"
+        :style="{
+            transform: cardTransform,
+            transformStyle: 'preserve-3d',
+            transition: 'transform 0.25s ease-out',
+            backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${blok.Image.filename})`,
+        }"
+        class="w-11/12 lg:w-3/4 xl:w-2/3 mx-auto font-sans text-neutral-200 text-center bg-cover will-change-transform py-32 lg:py-56 mt-24"
     >
-        {{ blok.Title }}
-        <div v-html="articleContent" />
-        {{ blok.Image.filename }}
+        <div
+            :style="{
+                transform: 'translate3d(0, 0, 48px)',
+                transformStyle: 'preserve-3d',
+            }"
+            className="position-relative z-10"
+        >
+            <h1
+                class="text-6xl font-bold mb-3"
+                v-text="blok.Title"
+            />
+            <div
+                class="w-5/6 xl:w-1/2 text-neutral-200 mx-auto"
+                v-html="articleContent"
+            />
+        </div>
     </div>
 </template>
