9ff9d80558b002b6431e9a5666b1083e328e7e12
Steven Vos <steven.vos@infocaster.net>
2023-08-14 07:33:29 +0200

    STVN > Added useSeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <script setup>
+    useSeoMeta({
+        title: "Steven Vos - Front-end Developer",
+        ogTitle: "Steven Vos - Front-end Developer",
+        description: "Hi, ik ben Steven. En ik maak websites en applicaties.",
+        ogDescription: "Hi, ik ben Steven. En ik maak websites en applicaties.",
+        // ogImage: 'https://example.com/image.png',
+        twitterCard: "summary_large_image",
+    });
+
     const story = await useAsyncStoryblok("home", { version: "draft" });
 </script>
 
