fe47907c9770356f664a181ad5c1b638ab8c8b3c
Steven Vos <steven.vos@infocaster.net>
2023-10-10 20:34:16 +0200

    STVN > Update to ServiceSlider + NavigationComponent + ServicesComponent

--- components/NavigationComponent.vue
+++ components/NavigationComponent.vue
@@ -14,7 +14,7 @@
 
 <template>
     <nav
-        class="fixed top-8 right-12 w-fit rounded-full border-2 py-2 px-3 bg-neutral-700/20 border-neutral-700/50"
+        class="fixed translate-x-1/2 lg:translate-x-0 right-1/2 bottom-8 lg:bottom-[unset] lg:top-8 lg:right-12 w-fit rounded-full border-2 py-2 px-3 bg-neutral-700/20 border-neutral-700/50"
     >
         <ul class="flex items-center gap-4">
             <li
--- storyblok/ServicesComponent.vue
+++ storyblok/ServicesComponent.vue
@@ -29,33 +29,12 @@
             "<"
         );
 
-        // const sTl = gsap.timeline({
-        //     scrollTrigger: {
-        //         trigger: `#${props.id}`,
-        //         start: "top 30%",
-        //         end: "top 10%",
-        //         scrub: 1,
-        //     },
-        // });
-
-        // sTl.fromTo(
-        //     `#service-${props.index}-0`,
-        //     { autoAlpha: 0, scale: 0.2 },
-        //     { autoAlpha: 1, scale: 1, duration: 1 },
-        //     "<"
-        // );
-
-        // for (let i = 0; i <= props.total; i++) {
-        //     sTl.fromTo(
-        //         `#service-${props.index}-${i}`,
-        //         { autoAlpha: 0, scale: 0.2 },
-        //         { autoAlpha: 1, scale: 1, duration: 1 },
-        //         i === 1 ? "<" : ">-0.5"
-        //     );
-        // }
-        onMounted(() => {
-            console.log(props.blok);
-        });
+        tl.fromTo(
+            `#service-slider`,
+            { autoAlpha: 0 },
+            { autoAlpha: 1, duration: 0.5 },
+            "<0.5"
+        );
     });
 </script>
 
@@ -64,10 +43,10 @@
         v-if="blok"
         :data-screen-component="blok.id"
         :id="blok.id"
-        class="min-h-screen flex flex-col justify-center text-white bg-black border-t border-t-neutral-700/40"
+        class="min-h-screen flex flex-col md:justify-center text-white bg-black border-t border-t-neutral-700/40 pt-12 pb-2 md:py-0"
     >
         <div class="container flex flex-col">
-            <div class="w-1/2">
+            <div class="md:w-1/2">
                 <h2
                     class="text-6xl mb-4"
                     :id="`title-${blok._uid}`"
@@ -79,14 +58,11 @@
                     v-html="blok.intro"
                 />
             </div>
-            <div class="mt-16">
+            <div
+                id="service-slider"
+                class="mt-16"
+            >
                 <service-slider :items="blok.services" />
-                <!-- <service-component
-                    v-for="service in blok.services"
-                    :id="`service-${blok._uid}`"
-                    :key="`service-${blok._uid}`"
-                    :item="service"
-                /> -->
             </div>
         </div>
     </section>
