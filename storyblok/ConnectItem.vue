458dc6db3cba7d1c1cc64f0f493242734345b4f8
Steven Vos <steven.vos@infocaster.net>
2023-11-19 12:18:46 +0100

    STVN > Changed font-width of ConnectItems

--- storyblok/ConnectItem.vue
+++ storyblok/ConnectItem.vue
@@ -9,12 +9,12 @@
         :href="blok.Link"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex items-center group text-neutral-400 dark:text-neutral-500 mb-4 md:mb-3 w-fit"
+        class="flex items-center group text-neutral-400 dark:text-neutral-900 mb-4 md:mb-3 w-fit"
     >
-        <span 
-            class="font-medium underline-hover after:bg-neutral-400 dark:after:bg-neutral-500"
+        <span
+            class="font-thin underline-hover after:bg-neutral-400 dark:after:bg-neutral-900"
             v-text="blok.Label"
         />
-        <span v-html="blok.Icon"/>
+        <span v-html="blok.Icon" />
     </a>
 </template>
