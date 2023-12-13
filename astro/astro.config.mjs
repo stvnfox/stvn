d8fe27ec2dc4158cbed228b4c680cb518d8c9975
Steven Vos <steven.vos@infocaster.net>
2023-12-13 19:48:58 +0100

    STVN > Add Tailwind and stylesheet + Start Storyblok integration + FooterComponent as Astro component

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                page: "storyblok/Page",
+            },
+        }),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
+});
