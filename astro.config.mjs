db406830e7c8dbc359c7fd0c334a90a0f01fb1c4
Steven Vos <steven.vos@infocaster.net>
2023-12-14 14:54:35 +0100

    STVN > Add React for ColorModeSwitcher component + ColorModeSwitcher implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,18 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
-
+import react from "@astrojs/react";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                page: "storyblok/Page",
-            },
-        }),
-        tailwind({
-            applyBaseStyles: false,
-        }),
-    ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: "storyblok/Page"
+    }
+  }), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
