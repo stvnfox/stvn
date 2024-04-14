f2a4599b016a889fe309090d27ee5fe3a2c098da
Steven <steven@stvnx.nl>
2024-04-14 08:54:57 +0200

    feat: add Sanity Vision plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { PageType } from "./sanity/types/page.types";
 
 export default defineConfig({
   name: "stvn",
   title: "STVN",
   projectId: "4xy3zla7",
   dataset: "production",
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool()],
   schema: {
-    types: [],
+    types: [PageType],
   },
 });
