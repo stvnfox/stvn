85b99c9289059eb702aa7ef544be6b29e1aa33a8
steven.vos <steven.vos@iodigital.com>
2024-05-15 21:04:33 +0200

    style(base): add some base styles and changed structure of page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+        fontFamily: {
+            sans: ["Lato", "sans-serif"],
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
+        extend: {},
+    },
 	plugins: [],
 }
