7f7e6995df06a86bd3f9a92fff881569811ee5bd
steven.vos <steven.vos@iodigital.com>
2024-05-15 20:39:48 +0200

    chore(packages): update packages and add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "4xy3zla7",
-      dataset: "production",
-      useCdn: false,
-      apiVersion: "2024-04-13",
-      studioBasePath: "/admin",
-    }),
-    react(),
-  ],
+  integrations: [sanity({
+    projectId: "4xy3zla7",
+    dataset: "production",
+    useCdn: false,
+    apiVersion: "2024-04-13",
+    studioBasePath: "/admin"
+  }), react(), tailwind()]
 });
