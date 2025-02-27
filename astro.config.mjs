8b3fd6895ae6e79e823eec7cbac248d16338e3a2
steven.vos <steven.vos@iodigital.com>
2025-02-27 09:22:32 +0100

    feat(sanity): add true as useCdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,20 +8,25 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: "4xy3zla7",
-    dataset: "production",
-    useCdn: false,
-    apiVersion: "2024-04-13",
-    studioBasePath: "/admin"
-  }), react(), tailwind({
-    applyBaseStyles: false,
-    nesting: true
-  }), icon()],
+  integrations: [
+    sanity({
+      projectId: "4xy3zla7",
+      dataset: "production",
+      useCdn: true,
+      apiVersion: "2024-04-13",
+      studioBasePath: "/admin",
+    }),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    icon(),
+  ],
   output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
-  })
+  }),
 });
